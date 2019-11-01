028bef182dccefffdb632f7838b98522c5c41826
Daniel Kanangila <daniel.kanangila@gmail.com>
2019-10-31 23:33:10 -0400

    Updated getFullNames

--- index.js
+++ index.js
@@ -180,8 +180,13 @@ function processDuplicateFree(list, callback) {
  * @returns an array with all the runners' full names in the following format: "Smith, John".
  * The full names appear in the array in the same order the runners appear in the `runners` array.
  */
-function getFullNames( /* CODE HERE */ ) {
-    /* CODE HERE */
+function getFullNames(runners) {
+    let fullNames = []
+    runners.forEach(
+        item => fullNames.push(`${item.last_name}, ${item.first_name}`)
+    );
+
+    return fullNames
 }
 
 /**
